f64efb17879b88d882f45d4640bf545241fe01b3
jonusis <1624100548@qq.com>
2020-10-11 23:44:25 +0800

    AC

--- 剑指offer/task6.java
+++ 剑指offer/task6.java
@@ -1,2 +1,53 @@
 /**
+用两个栈实现一个队列。队列的声明如下，请实现它的两个函数 appendTail 和 deleteHead ，分别完成在队列尾部插入整数和在队列头部删除整数的功能。(若队列中没有元素，deleteHead 操作返回 -1 )
+
+ 
+
+示例 1：
+
+输入：
+["CQueue","appendTail","deleteHead","deleteHead"]
+[[],[3],[],[]]
+输出：[null,null,3,-1]
+示例 2：
+
+输入：
+["CQueue","deleteHead","appendTail","appendTail","deleteHead","deleteHead"]
+[[],[],[5],[2],[],[]]
+输出：[null,-1,null,null,5,2]
+
+ */
+
+
+class CQueue {
+    Stack <Integer> stack1 = new Stack<>();
+    Stack <Integer> stack2 = new Stack<>(); 
+    public CQueue() {
+        Stack<Integer> stack1 = new Stack<>();
+        Stack<Integer> stack2 = new Stack<>();
+    }
+    
+    public void appendTail(int value) {
+        stack1.push(value);
+    }
+    
+    public int deleteHead() {
+        if(stack2.isEmpty()){
+            while(!stack1.isEmpty()){
+                stack2.push(stack1.pop());
+            }
+        }
+        if(!stack2.isEmpty()){
+            int res = stack2.pop();
+            return res;  
+        }
+    return -1;
+    }
+}
+
+/**
+ * Your CQueue object will be instantiated and called as such:
+ * CQueue obj = new CQueue();
+ * obj.appendTail(value);
+ * int param_2 = obj.deleteHead();
  */
