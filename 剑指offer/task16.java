9185338c8aeace9a72d57ac03210e54edc24ac41
jonusis <1624100548@qq.com>
2020-10-17 22:54:41 +0800

    AC

--- 剑指offer/task16.java
+++ 剑指offer/task16.java
@@ -1,2 +1,31 @@
 /**
+输入数字 n，按顺序打印出从 1 到最大的 n 位十进制数。比如输入 3，则打印出 1、2、3 一直到最大的 3 位数 999。
+
+示例 1:
+
+输入: n = 1
+输出: [1,2,3,4,5,6,7,8,9]
+
  */
+ class Solution {
+    public int[] printNumbers(int n) {
+        int num = 1;
+            switch(n){
+                case 1:num = 9;break;
+                case 2:num = 99;break;
+                case 3:num = 999;break;
+                case 4:num = 9999;break;
+                case 5:num = 99999;break;
+                case 6:num = 999999;break;
+                case 7:num = 9999999;break;
+                case 8:num = 99999999;break;
+                case 9:num = 999999999;break;
+            }
+            int[] arr = null;
+            arr = new int[num];
+            for(int i = 0;i < num;i++){
+                arr[i] = i+1;
+            }
+            return arr;
+    }
+}
--- 剑指offer/task17.java
+++ 剑指offer/task17.java
@@ -1,2 +1,46 @@
 /**
+剑指 Offer 18. 删除链表的节点
+给定单向链表的头指针和一个要删除的节点的值，定义一个函数删除该节点。
+
+返回删除后的链表的头节点。
+
+注意：此题对比原题有改动
+
+示例 1:
+
+输入: head = [4,5,1,9], val = 5
+输出: [4,1,9]
+解释: 给定你链表中值为 5 的第二个节点，那么在调用了你的函数之后，该链表应变为 4 -> 1 -> 9.
+示例 2:
+
+输入: head = [4,5,1,9], val = 1
+输出: [4,5,9]
+解释: 给定你链表中值为 1 的第三个节点，那么在调用了你的函数之后，该链表应变为 4 -> 5 -> 9.
  */
+
+ /**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode deleteNode(ListNode head, int val) {
+        if(head.next == null) return null;
+        ListNode p = head;
+        ListNode q = head.next;
+        if(p.val == val) return q;
+        while(q!=null){
+            if(q.val == val){
+                p.next = q.next;
+                q = p.next;
+                break;
+            }
+            p = p.next;
+            q = q.next;
+        }
+        return head;
+    }
+}
