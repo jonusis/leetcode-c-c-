afb58ffc4ec9e1ef6db9386f6535908602932ae3
jonusis <1624100548@qq.com>
2020-11-28 22:36:36 +0800

    new list

---
+++ 剑指offer/剑指 Offer 40. 最小的k个数.java
@@ -0,0 +1,26 @@
+/**
+剑指 Offer 40. 最小的k个数
+输入整数数组 arr ，找出其中最小的 k 个数。例如，输入4、5、1、6、2、7、3、8这8个数字，则最小的4个数字是1、2、3、4。
+
+示例 1：
+
+输入：arr = [3,2,1], k = 2
+输出：[1,2] 或者 [2,1]
+示例 2：
+
+输入：arr = [0,1,2,1], k = 1
+输出：[0]
+ */
+class Solution {
+    public int[] getLeastNumbers(int[] arr, int k) {
+        Queue<Integer> pq = new PriorityQueue<Integer>();
+        for(int i = 0;i < arr.length;i++){
+            pq.offer(arr[i]);
+        }
+        int res[] = new int[k];
+        for(int i = 0;i < k;i++){
+            res[i] = pq.poll();
+        }
+        return res;
+    }
+}
