b7d85fe269c1129b8cf53ada025717a07b7c0aeb
jonusis <1624100548@qq.com>
2020-12-10 18:56:15 +0800

    new list

--- 剑指offer/剑指 Offer 56 - II. 数组中数字出现的次数 II.java
+++ 剑指offer/剑指 Offer 56 - II. 数组中数字出现的次数 II.java
@@ -1,3 +1,35 @@
 /**
- * 
+剑指 Offer 56 - II. 数组中数字出现的次数 II
+在一个数组 nums 中除一个数字只出现一次之外，其他数字都出现了三次。请找出那个只出现一次的数字。
+
+ 
+
+示例 1：
+
+输入：nums = [3,4,3,3]
+输出：4
+示例 2：
+
+输入：nums = [9,1,7,9,7,9,7]
+输出：1
  */
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        HashMap<Integer,Boolean> map = new HashMap<>();
+        int len = nums.length;
+        for(int i = 0;i < len;i++){
+            if(!map.containsKey(nums[i])){
+                map.put(nums[i],true);
+            }else{
+                map.put(nums[i],false);
+            }
+        }
+        for(int i = 0;i < len;i++){
+            if(map.get(nums[i])){
+                return nums[i];
+            }
+        }
+        return nums[0];
+    }
+}
