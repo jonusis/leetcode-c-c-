679470e4fce6a865fc03f6a3431dfab4a9811a2b
jonusis <1624100548@qq.com>
2020-12-14 14:44:59 +0800

    new list

---
+++ leetcode热题100/152. 乘积最大子数组.java
@@ -0,0 +1,36 @@
+/**
+152. 乘积最大子数组
+给你一个整数数组 nums ，请你找出数组中乘积最大的连续子数组（该子数组中至少包含一个数字），并返回该子数组所对应的乘积。
+
+ 
+
+示例 1:
+
+输入: [2,3,-2,4]
+输出: 6
+解释: 子数组 [2,3] 有最大乘积 6。
+示例 2:
+
+输入: [-2,0,-1]
+输出: 0
+解释: 结果不能为 2, 因为 [-2,-1] 不是子数组。
+ */
+
+class Solution {
+    public int maxProduct(int[] nums) {
+        int nowMax = nums[0];
+        int nowMin = nums[0];
+        int premax = nowMax;
+        int len = nums.length;
+        int max = nowMax;
+        for(int i = 1;i < len;i++){
+            premax = nowMax;
+            nowMax = Math.max(nums[i] * nowMax,Math.max(nums[i] * nowMin,nums[i]));
+            nowMin = Math.min(nums[i] * premax,Math.min(nums[i] * nowMin,nums[i]));
+            if(nowMax > max){
+                max = nowMax;
+            }
+        }
+        return max;
+    }
+}
